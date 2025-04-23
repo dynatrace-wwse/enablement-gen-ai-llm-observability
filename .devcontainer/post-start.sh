208b94c673c47e272482738b6262bdce30ac50e1
Giovanni Liva <giovanni.liva@dynatrace.com>
2025-04-23 09:26:16 +0200

    switch app

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -8,7 +8,7 @@ source /workspaces/$RepositoryName/.devcontainer/util/functions.sh
 
 #TODO: BeforeGoLive comment this so the Mkdocs are not exposed in the container.
 # we want to monitor all interactions of the users in the live github pages.
-exposeMkdocs
+#exposeMkdocs
 
 # In here you can add steps or configure things for the training
 
