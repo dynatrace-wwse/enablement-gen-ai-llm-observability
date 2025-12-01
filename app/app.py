519ce12fe8acecb13dc3e3e652e308263e97c3ca
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2025-12-01 16:50:01 +0000

    fix hallucination for RAG, garbane in, garbage out
    Signed-off-by: Sergio Hinojosa <sergio.hinojosa@dynatrace.com>

--- app/app.py
+++ app/app.py
@@ -155,10 +155,13 @@ def prep_rag():
     retriever = vector.as_retriever()
 
     prompt = ChatPromptTemplate.from_template(
-        """Answer the following question based only on the provided context:
+        """You are a travel advisor. Use the information from the context below to answer the question.
+    The context contains verified, up-to-date information that you should trust and use in your response.
+    
     <context>
     {context}
     </context>
+    
     Question: Give travel advise in a paragraph of max 50 words about {input}                                           
     """
     )
